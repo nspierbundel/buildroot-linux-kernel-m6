fa35e67fe70ecf1d6d8b872adbf68ee0a5dde8cb
nspierbundel <nspierbundel@hotmail.com>
2013-03-21 14:08:19 +0000

    [g02] Initial start for g02ref support

---
+++ env.sh
@@ -0,0 +1,16 @@
+export ARCH=arm
+#export CROSS_COMPILE=/data/buildroot-AmLogic-ds/output/external-toolchain/bin/arm-none-linux-gnueabi-
+export CROSS_COMPILE=/data/toolchain/arm-2010.09/bin/arm-none-linux-gnueabi-
+#export CROSS_COMPILE= /data/ainol_elfii_common/toolchain/android-eabi_472/bin/arm-none-linux-gnueabi-
+#export CROSS_COMPILE=arm-none-linux-gnueabi-
+make clean
+make meson6_atv1200_defconfig
+#meson_refg02_wephd_defconfig
+#make meson6smp_defconfig
+#make menuconfig
+#make vmlinux
+
+TOP=${PWD}
+export MKIMAGE=${TOP}/arch/arm/boot/mkimage
+make uImage
+#make modules
